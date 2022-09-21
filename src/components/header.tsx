d64fa5253279a7df166e1dcd169cc88a63088d17
takahiro2007 <watataka2007728@gmail.com>
2022-09-21 21:34:10 +0900

    make header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,48 @@
 import { css } from "@emotion/react";
 import { useRouter } from "next/router";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 const Header = () => {
-  const router = useRouter()
+  const router = useRouter();
   return (
-    <div>
+    <div css={headerContent}>
       <p>Watakablog</p>
-      <div>
-        <button onClick={() => router.push("/")}>Category</button>
-        <button onClick={() => router.push("/")}>All_Articles</button>
-        <button onClick={() => router.push("/")}>About</button>
-      </div>
+      <ButtonGroup>
+        <Button
+          onClick={() => router.push("/")}
+          colorScheme="teal"
+          variant="ghost"
+        >
+          Category
+        </Button>
+        <Button
+          onClick={() => router.push("/")}
+          colorScheme="teal"
+          variant="ghost"
+        >
+          All_Articles
+        </Button>
+        <Button
+          onClick={() => router.push("/")}
+          colorScheme="teal"
+          variant="ghost"
+        >
+          About
+        </Button>
+      </ButtonGroup>
     </div>
   );
 };
 
+const headerContent = css`
+  width: 100vw;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 60px;
+  background-color: white;
+  color: black;
+  box-shadow: 0 0px 5px 0;
+`;
 
 export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,12 @@ import Layout from "../components/layout";
 
 const Home = () => {
   return (
-  <Layout>
-<div>
-  <h1>a</h1>
-    </div>
-  </Layout>
+    <Layout>
+      <div>
+        <h1>a</h1>
+      </div>
+    </Layout>
   );
 };
-
 
 export default Home;
